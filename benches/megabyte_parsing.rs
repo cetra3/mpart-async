f69135c23e778d4a1e4a43cb30d39cfb6713b289
cetra3 <cetra3@hotmail.com>
2020-07-16 09:47:16 +0930

    Include new benchmark

--- benches/megabyte_parsing.rs
+++ benches/megabyte_parsing.rs
@@ -1,59 +1,29 @@
-use bytes::{BufMut, Bytes, BytesMut};
+use bytes::{Bytes, BytesMut};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use futures::executor::block_on;
+use futures::future;
 use futures::stream::{Stream, StreamExt};
+use mpart_async::client::{ByteStream, MultipartRequest};
 use mpart_async::server::MultipartStream;
+use rand::{thread_rng, RngCore};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let boundary = b"----------------------------332056022174478975396798";
+fn ten_megabytes_zero_byte(c: &mut Criterion) {
+    let total_size = 1024 * 1024 * 10;
 
-    let mut buffer = Vec::with_capacity(10 * 1024 * 1024);
-    buffer.extend_from_slice(b"--");
-    buffer.extend_from_slice(boundary);
-    buffer.extend_from_slice(b"\r\n");
-    buffer.extend_from_slice(b"Content-Disposition: form-data; name=\"file\"\r\n");
-    buffer.extend_from_slice(b"Content-Type: application/octet-stream\r\n");
-    buffer.extend_from_slice(b"\r\n");
+    let (bytes, boundary) = bytes_and_boundary(&['\0' as u8; 1024 * 1024 * 10]);
 
-    // formidable uses just zeroes so I guess that's good enough:
-    // https://github.com/node-formidable/formidable/blob/5110ef8ddb78501dcedbdcb7e2754d94abe06bc5/benchmark/index.js#L45
-
-    let mut zeroes = BytesMut::with_capacity(1024);
-    for _ in 0..(zeroes.capacity() / 8) {
-        zeroes.put_u64(0);
-    }
-
-    let trailer = 2 + boundary.len() + 2;
-    let target = buffer.capacity();
-    let mut remaining = target - (buffer.len() + trailer);
-    let zeroes_used = remaining;
-
-    while remaining > zeroes.len() {
-        buffer.extend_from_slice(zeroes.as_ref());
-        remaining -= zeroes.len();
-    }
-
-    buffer.extend_from_slice(&zeroes.as_ref()[..remaining]);
-
-    buffer.extend_from_slice(b"\r\n--");
-    buffer.extend_from_slice(boundary);
-    buffer.extend_from_slice(b"--\r\n");
-
-    let boundary: Bytes = (&boundary[..]).into();
-    let bytes: Bytes = buffer.into();
-
-    let mut group = c.benchmark_group("ten megabytes");
+    let mut group = c.benchmark_group("ten megabytes zero byte");
 
     // with bigger chunk sizes this does get a lot of faster, but it would perhaps be better to
     // have multiple smaller files
-    for chunk_size in &[512] {
-        group.throughput(criterion::Throughput::Bytes(target as u64));
+    for chunk_size in &[512, 1024, 2048] {
+        group.throughput(criterion::Throughput::Bytes(total_size as u64));
 
         group.bench_with_input(
             criterion::BenchmarkId::from_parameter(chunk_size),
-            &zeroes_used,
+            &total_size,
             |b, &size| {
                 b.iter(|| {
                     let bytes = black_box(count_single_field_bytes(
@@ -62,11 +32,98 @@ fn criterion_benchmark(c: &mut Criterion) {
                         size,
                     ));
 
-                    assert_eq!(bytes, zeroes_used);
+                    assert_eq!(bytes, total_size);
                 });
             },
         );
     }
+}
+
+fn ten_megabytes_r_byte(c: &mut Criterion) {
+    let total_size = 1024 * 1024 * 10;
+
+    let (bytes, boundary) = bytes_and_boundary(&['\r' as u8; 1024 * 1024 * 10]);
+
+    let mut group = c.benchmark_group("ten megabytes \\r byte");
+
+    // with bigger chunk sizes this does get a lot of faster, but it would perhaps be better to
+    // have multiple smaller files
+    for chunk_size in &[512, 1024, 2048] {
+        group.throughput(criterion::Throughput::Bytes(total_size as u64));
+
+        group.bench_with_input(
+            criterion::BenchmarkId::from_parameter(chunk_size),
+            &total_size,
+            |b, &size| {
+                b.iter(|| {
+                    let bytes = black_box(count_single_field_bytes(
+                        boundary.clone(),
+                        bytes.clone(),
+                        size,
+                    ));
+
+                    assert_eq!(bytes, total_size);
+                });
+            },
+        );
+    }
+}
+
+fn ten_megabytes_random(c: &mut Criterion) {
+    let total_size = 1024 * 1024 * 10;
+
+    let mut bytes_mut = BytesMut::with_capacity(total_size);
+
+    bytes_mut.extend_from_slice(&['\0' as u8; 1024 * 1024 * 10]);
+
+    thread_rng().fill_bytes(&mut bytes_mut);
+
+    let (bytes, boundary) = bytes_and_boundary(&bytes_mut);
+
+    let mut group = c.benchmark_group("ten megabytes random");
+
+    // with bigger chunk sizes this does get a lot of faster, but it would perhaps be better to
+    // have multiple smaller files
+    for chunk_size in &[512, 1024, 2048] {
+        group.throughput(criterion::Throughput::Bytes(total_size as u64));
+
+        group.bench_with_input(
+            criterion::BenchmarkId::from_parameter(chunk_size),
+            &total_size,
+            |b, &size| {
+                b.iter(|| {
+                    let bytes = black_box(count_single_field_bytes(
+                        boundary.clone(),
+                        bytes.clone(),
+                        size,
+                    ));
+
+                    assert_eq!(bytes, total_size);
+                });
+            },
+        );
+    }
+}
+
+fn bytes_and_boundary(input: &[u8]) -> (Bytes, Bytes) {
+    let mut request = MultipartRequest::default();
+
+    let byte_stream = ByteStream::new(input);
+
+    request.add_stream("name", "filename", "content_stream", byte_stream);
+
+    let boundary = Bytes::from(request.get_boundary().to_string());
+
+    let bytes = block_on(request.fold(BytesMut::new(), |mut buf, result| {
+        if let Ok(bytes) = result {
+            buf.extend_from_slice(&bytes);
+        };
+
+        future::ready(buf)
+    }))
+    .freeze();
+
+    (bytes, boundary)
 }
 
 fn count_single_field_bytes(boundary: Bytes, bytes: Bytes, chunk_size: usize) -> usize {
@@ -106,5 +163,10 @@ impl Stream for ChunkedStream {
     }
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(
+    benches,
+    ten_megabytes_zero_byte,
+    ten_megabytes_r_byte,
+    ten_megabytes_random
+);
 criterion_main!(benches);
